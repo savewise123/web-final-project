e692fdcbc8096ef6a4aa3ce09a3b2c295a96ab79
your name <you@example.com>
2025-01-03 12:25:04 +0900

    signup 구현

--- src/componenets/Layout.tsx
+++ src/componenets/Layout.tsx
@@ -1,12 +1,37 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Layout() {
+  const user = null;
+
   return (
     <>
-      <header>
-        헤더
-        <button class="btn btn-blue">로그인</button>
-        <button>회원가입</button>
+      <header className="flex justify-between bg-white p-4 border-b">
+        <Link to="/">
+          <img src="/logo12.svg" alt="logo12" />
+        </Link>
+        <div className="flex gap-5 items-center">
+          {user ? (
+            <>
+              <Link to="mypage">{user.nickname}</Link>
+              <button>로그아웃</button>
+            </>
+          ) : (
+            <>
+              <Link
+                to="/login"
+                className="text-gray-900 bg-white border border-gray-300 rounded-lg px-5 "
+              >
+                로그인
+              </Link>
+              <Link
+                to="/signup"
+                className="text-gray-900 bg-white border border-gray-300 rounded-lg px-5 px-3"
+              >
+                회원가입
+              </Link>
+            </>
+          )}
+        </div>
       </header>
       <Outlet />
     </>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,3 +1,53 @@
 export default function Signup() {
-  return <>회원가입</>;
+  return (
+    <Container>
+      <InputGroup>
+        <label htmlFor="email">이메일</label>
+        <input
+          type="text"
+          id="email"
+          placeholder=""
+          value={email}  
+          onChange={onEmailDate} 
+        />
+      </InputGroup>
+      <InputGroup>
+        <label htmlFor="nickname">닉네임</label>
+        <input
+          type="text"
+          id="nickname"
+          placeholder=""
+          value={nickname}
+          onChange={onChangeNickname}
+        />
+      </InputGroup>
+      <InputGroup>
+        <label htmlFor="password">비밀번호</label>
+        <input
+          type="number"
+          id="password"
+          placeholder=""
+          value={amount}
+          onChange={onChangeAmount}
+        />
+      </InputGroup>
+      <InputGroup>
+        <label htmlFor="passwordConfirm">비밀번호 확인인</label>
+        <input
+          type="text"
+          id="description"
+          placeholder=""
+          value={description}
+          onChange={onChangeDescription}
+        />
+      </InputGroup>
+      <ButtonGroup>
+        <Button onClick={onEdit}>수정</Button>
+        <Button onClick={onDelete} danger="true">
+          삭제
+        </Button>
+        <BackButton onClick={() => navigate(-1)}>뒤로 가기</BackButton>
+      </ButtonGroup>
+    </Container>
+  );
 }
